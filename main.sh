18f1e4a2b530718c13cff041d6613f757c372dee
Martin Murphy <martin@adaptive.co.uk>
2018-12-05 09:44:56 +0000

    Added all blank files for project.

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+
+source $DIR/commands/cd_and_ls.sh
+source $DIR/commands/hosts.sh
+source $DIR/commands/exp.sh
+source $DIR/commands/dx.sh
+source $DIR/commands/local_copy.sh
+source $DIR/commands/site_search.sh
+source $DIR/commands/adaptive_help.sh 
